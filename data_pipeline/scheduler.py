1b56f596d6f5533334220712e41d36b9e333ec5e
athithijain21-stack <athithijain21@gmail.com>
2026-05-08 23:55:13 +0530

    Finalized automated data pipeline and resolved ML dependencies

--- data_pipeline/scheduler.py
+++ data_pipeline/scheduler.py
@@ -4,9 +4,9 @@ import fetch_news
 def run():
     while True:
         print("🔄 Updating data...")
-        fetch_news()
-        print("⏳ Sleeping 30 min...\n")
-        time.sleep(1800)
+        fetch_news.fetch_news_rss(["TSLA", "AAPL", "GOOGL"])
+        print("⏳ Sleeping 1 min...\n")        
+        time.sleep(300)
 
 if __name__ == "__main__":
     run()
